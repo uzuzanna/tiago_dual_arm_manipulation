6a0f6063185bee875ce47dff1eba0a0c59b042fe
uzuzanna <uzuzia@wp.pl>
2026-01-24 16:51:49 +0100

    Delete src/object_segmentation/object_segmentation/cale.py

--- src/object_segmentation/object_segmentation/cale.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-import numpy as np
-cloud = o3d.io.read_point_cloud("points_20251020_184411.ply")
-R = cloud.get_rotation_matrix_from_xyz((np.pi, 0, 0))
-cloud.rotate(R, center=(0, 0, 0))
-
-o3d.visualization.draw_geometries([cloud])
-
